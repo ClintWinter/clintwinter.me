d1541f90d026e47cdf8f124f69ec94f4c8aaf162
Clint Winter <cwinter.web@gmail.com>
2023-08-06 19:59:02 -0400

    bump versions + style fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import torchlight from 'remark-torchlight';
 
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fresh.clintwinter.me',
-
-    integrations: [tailwind()],
-
-    markdown: {
-        remarkPlugins: [
-            [torchlight, { config: { theme: 'github-dark' }}],
-        ],
-        extendDefaultPlugins: true,
-    },
+  site: 'https://fresh.clintwinter.me',
+  integrations: [tailwind()],
+  markdown: {
+    remarkPlugins: [[torchlight, {
+      config: {
+        theme: 'github-dark'
+      }
+    }]],
+    extendDefaultPlugins: true
+  }
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,37 +7,9 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                'display': ['Abhaya Libre', ...defaultTheme.fontFamily.serif],
-                'sans': ['iA Writer Quattro', ...defaultTheme.fontFamily.sans],
-                'mono': ['jetbrains-mono', ...defaultTheme.fontFamily.mono],
-            },
-            colors: {
-                nord: {
-                    n: {
-                        '900': '#2E3440',
-                        '800': '#3B4252',
-                        '700': '#434C5E',
-                        '600': '#4C566A',
-                    },
-                    s: {
-                        '300': '#D8DEE9',
-                        '200': '#E5E9F0',
-                        '100': '#ECEFF4',
-                    },
-                    f: {
-                        green: '#8FBCBB',
-                        teal: '#88C0D0',
-                        gray: '#81A1C1',
-                        slate: '#5E81AC',
-                    },
-                    a: {
-                        red: '#BF616A',
-                        orange: '#D08770',
-                        yellow: '#EBCB8B',
-                        green: '#738E5C',
-                        purple: '#B48EAD',
-                    }
-                },
+                'display': ['IBM Plex Sans', ...defaultTheme.fontFamily.serif],
+                'sans': ['IBM Plex Sans', ...defaultTheme.fontFamily.sans],
+                'mono': ['Iosevka', ...defaultTheme.fontFamily.mono],
             },
         },
     },
